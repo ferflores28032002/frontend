60daf7c3d34467af51e10f706604f63b62acce7e
Fernando José Flores Mendoza <110640664+ferflores28032002@users.noreply.github.com>
2024-11-23 16:00:45 -0600

    se agrego el data.email

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    proxy: {
+      // Redirige las solicitudes que comiencen con "/api" al backend
+      "/api": {
+        target: "http://www.registroreparacionesmantenimientos.somee.com", // URL de tu backend
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""), // Elimina el prefijo "/api"
+      },
+    },
+  },
+});
