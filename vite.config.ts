4277d7f283d6ed647502cafa427e35c9dbada8fe
Fernando José Flores Mendoza <110640664+ferflores28032002@users.noreply.github.com>
2024-11-23 16:07:45 -0600

    dhbfhbejhwb

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://www.registroreparacionesmantenimientos.somee.com",
+        target: "https://www.registroreparacionesmantenimientos.somee.com",
         changeOrigin: true,
         secure: false, // Permite HTTP inseguro
         rewrite: (path) => path.replace(/^\/api/, ""),
