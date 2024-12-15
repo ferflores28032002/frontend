a3f42f00b1b45e1a59861d8c8c682d1768cc3349
Fernando José Flores Mendoza <110640664+ferflores28032002@users.noreply.github.com>
2024-12-15 09:13:27 -0600

    khwbehjb

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
   BrowserRouter,
   Route,
-  Routes,
-  Navigate,
-  Outlet,
-  useLocation,
+  Routes
 } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import { DataTableEquipos } from "./components/Equipos/data-table";
+import { DataTableMantenimiento } from "./components/Mantenimientos/data-table";
+import { DataTableReparaciones } from "./components/Reparaciones/data-table";
+import { DataTable } from "./components/Users/data-table";
 import Layout from "./Layout";
 import { LoginPage } from "./pages";
-import Dashboard from "./components/Dashboard";
-import { DataTable } from "./components/Users/data-table";
-import { useMainStore } from "./store";
-import { DataTableMantenimiento } from "./components/Mantenimientos/data-table";
-import { DataTableEquipos } from "./components/Equipos/data-table";
-import { DataTableReparaciones } from "./components/Reparaciones/data-table";
-
-// Tipos para las props del componente ProtectedRoute
-interface ProtectedRouteProps {
-  isAllowed: boolean;
-  redirectTo?: string;
-}
-
-// Componente de Ruta Protegida
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
-  isAllowed,
-  redirectTo = "/login",
-}) => {
-  const location = useLocation();
-
-  useEffect(() => {
-    if (!isAllowed) {
-      console.warn(`Acceso denegado a la ruta: ${location.pathname}`);
-    }
-  }, [isAllowed, location.pathname]);
-
-  return isAllowed ? <Outlet /> : <Navigate to={redirectTo} replace />;
-};
 
 const App: React.FC = () => {
-  const user = useMainStore((state) => state.user);
+ 
 
   return (
     <BrowserRouter>
